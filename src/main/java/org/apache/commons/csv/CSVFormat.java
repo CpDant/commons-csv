623cd29359c47526681d2015c428134d9077d942
Ivan Capobianco <capobianco.ivan@gmail.com>
2023-11-30 10:21:08 +0100

    Reduce Cognitive Complexity in CSVFormat.java

--- src/main/java/org/apache/commons/csv/CSVFormat.java
+++ src/main/java/org/apache/commons/csv/CSVFormat.java
@@ -2460,31 +2460,59 @@ public final class CSVFormat implements Serializable {
             throw new IllegalArgumentException("The delimiter cannot be a line break");
         }
 
-        if (quoteCharacter != null && contains(delimiter, quoteCharacter.charValue())) {
+        if (areQuoteCharAndDelimiterEqual(quoteCharacter, delimiter)) {
             throw new IllegalArgumentException("The quoteChar character and the delimiter cannot be the same ('" + quoteCharacter + "')");
         }
 
-        if (escapeCharacter != null && contains(delimiter, escapeCharacter.charValue())) {
+        if (areEscapeCharAndDelimiterEqual(escapeCharacter, delimiter)) {
             throw new IllegalArgumentException("The escape character and the delimiter cannot be the same ('" + escapeCharacter + "')");
         }
 
-        if (commentMarker != null && contains(delimiter, commentMarker.charValue())) {
+        if (areCommentStartCharAndDelimiterEqual(commentMarker, delimiter)) {
             throw new IllegalArgumentException("The comment start character and the delimiter cannot be the same ('" + commentMarker + "')");
         }
 
-        if (quoteCharacter != null && quoteCharacter.equals(commentMarker)) {
+        if (areCommentStartCharAndQuoteCharEqual(quoteCharacter, commentMarker)) {
             throw new IllegalArgumentException("The comment start character and the quoteChar cannot be the same ('" + commentMarker + "')");
         }
 
-        if (escapeCharacter != null && escapeCharacter.equals(commentMarker)) {
+        if (areCommentStartCharAndEscapeCharEqual(escapeCharacter, commentMarker)) {
             throw new IllegalArgumentException("The comment start and the escape character cannot be the same ('" + commentMarker + "')");
         }
 
-        if (escapeCharacter == null && quoteMode == QuoteMode.NONE) {
+        if (isEscapeCharSetForQuoteModeNone(escapeCharacter, quoteMode)) {
             throw new IllegalArgumentException("Quote mode set to NONE but no escape character is set");
         }
 
         // Validate headers
+        validateHeaders();
+    }
+
+    private boolean areQuoteCharAndDelimiterEqual(Character quoteCharacter, String delimiter) {
+        return quoteCharacter != null && contains(delimiter, quoteCharacter.charValue());
+    }
+
+    private boolean areEscapeCharAndDelimiterEqual(Character escapeCharacter, String delimiter) {
+        return escapeCharacter != null && contains(delimiter, escapeCharacter.charValue());
+    }
+
+    private boolean areCommentStartCharAndDelimiterEqual(Character commentMarker, String delimiter) {
+        return commentMarker != null && contains(delimiter, commentMarker.charValue());
+    }
+
+    private boolean areCommentStartCharAndQuoteCharEqual(Character quoteCharacter, Character commentMarker) {
+        return quoteCharacter != null && quoteCharacter.equals(commentMarker);
+    }
+
+    private boolean areCommentStartCharAndEscapeCharEqual(Character escapeCharacter, Character commentMarker) {
+        return escapeCharacter != null && escapeCharacter.equals(commentMarker);
+    }
+
+    private boolean isEscapeCharSetForQuoteModeNone(Character escapeCharacter, QuoteMode quoteMode) {
+        return escapeCharacter == null && quoteMode == QuoteMode.NONE;
+    }
+
+    private void validateHeaders() throws IllegalArgumentException {
         if (headers != null && duplicateHeaderMode != DuplicateHeaderMode.ALLOW_ALL) {
             final Set<String> dupCheckSet = new HashSet<>(headers.length);
             final boolean emptyDuplicatesAllowed = duplicateHeaderMode == DuplicateHeaderMode.ALLOW_EMPTY;
@@ -2494,9 +2522,9 @@ public final class CSVFormat implements Serializable {
                 final boolean containsHeader = !dupCheckSet.add(blank ? "" : header);
                 if (containsHeader && !(blank && emptyDuplicatesAllowed)) {
                     throw new IllegalArgumentException(
-                        String.format(
-                            "The header contains a duplicate name: \"%s\" in %s. If this is valid then use CSVFormat.Builder.setDuplicateHeaderMode().",
-                            header, Arrays.toString(headers)));
+                            String.format(
+                                    "The header contains a duplicate name: \"%s\" in %s. If this is valid then use CSVFormat.Builder.setDuplicateHeaderMode().",
+                                    header, Arrays.toString(headers)));
                 }
             }
         }
